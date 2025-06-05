2d524433ce6373ba10a353573a624b7df13ba1cb
Yash Thakur <yashthakur.1808@gmail.com>
2025-06-05 09:16:22 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,21 @@
+console.log("Hello world");
+console.log("This is a simple Node.js application.");
+for (let i=0; i<44; i++) 
+    {
+     console.log("Current count: ${i}");
+    }
+let string = ("thus the High Guard has risen");
+console.log('string length:', string.length);
+console.log("hola");
+
+for (let i=1; i<=20; i++)
+{
+    if(i % 2 == 0)
+    {
+        console.log(i + " is even");
+    }
+    else
+    {
+        console.log(i + " is odd");
+    }
+}
