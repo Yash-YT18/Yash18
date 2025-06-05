49fce8ff6738beab419951c7257985044ba9a0c4
Yash Thakur <yashthakur.1808@gmail.com>
2025-06-05 10:37:08 +0530

    i learned function

---
+++ Yash05.js
@@ -0,0 +1,19 @@
+//const prompt = require('prompt-sync')();
+//const name = prompt("What is your name? ");
+//console.log("Hello, " + name + "!")
+
+//function sq(x)
+//{
+  //  let ans = 0;
+    //ans = x*x;
+    //return ans;
+//}
+//console.log("The Ans is : " + sq(12));
+
+const xyz = function (x=5)
+{
+    let ans = 0;
+    ans = x*x;
+    return ans;
+}
+console.log("The Ans is : " + xyz(9));
