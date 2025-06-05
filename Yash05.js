39da5d8a9a3ca4beaa5e8a946c79816b581fbf8c
Yash Thakur <yashthakur.1808@gmail.com>
2025-06-05 11:16:44 +0530

    imp callbacks

--- Yash05.js
+++ Yash05.js
@@ -10,10 +10,24 @@
 //}
 //console.log("The Ans is : " + sq(12));
 
-const xyz = function (x=5)
-{
-    let ans = 0;
-    ans = x*x;
-    return ans;
-}
-console.log("The Ans is : " + xyz(9));
+//const xyz = function (x=5)
+//{
+  //  let ans = 0;
+  //  ans = x*x;
+  //  return ans;
+//}
+//console.log("The Ans is : " + xyz(9));
+
+//function greet(name, cb)
+//{
+//  console.log("hello, " + name + "!");
+//  cb();
+//}
+
+//function gdb()
+//{
+//  console.log("Goodbye!");
+//}
+
+//greet("Yash", gdb);
+
